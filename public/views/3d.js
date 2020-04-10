c7f665451ac3e762de4fa64cb91e77f4c8dc18ca
sol <mssarratea@gmail.com>
2020-04-09 21:27:07 -0400

    Add  julia set distance estimator function, orbit and drag controls

--- public/views/3d.js
+++ public/views/3d.js
@@ -17,10 +17,27 @@ var uniforms = {
 }
 
 var gui = new dat.GUI();
-var guiData = {"camX": 0., "camY": 0., "camZ": -0.1, "zoom": -1.5, "colorA": [255,0,0], "colorB":[0,0,255], "t": 0.5, "power":5., "rotate": 0.};
+var guiData = { 
+  "cR": 0.0,
+  "cI": 0.0,
+  "cJ": 0.0,
+  "cK": 0.0,
+  "camX": 0., 
+  "camY": 0., 
+  "camZ": -0.1, 
+  "zoom": -1.5, 
+  "colorA": [255,0,0], 
+  "colorB":[0,0,255], 
+  "t": 0.5, 
+  "power":5., 
+  "rotate": 0.,
+};
 
-// gui.add(guiData, 'cR', -2., 2.).step(0.001);
-// gui.add(guiData, 'cI', -2., 2.).step(0.001);
+var rotationFlag =false;
+gui.add(guiData, 'cR', -2., 2.).step(0.001);
+gui.add(guiData, 'cI', -2., 2.).step(0.001);
+gui.add(guiData, 'cJ', -2., 2.).step(0.001);
+gui.add(guiData, 'cK', -2., 2.).step(0.001);
 gui.add(guiData, 'camX', -.2, .2).step(0.001);
 gui.add(guiData, 'camY', -.2, .2).step(0.001);
 gui.add(guiData, 'camZ', -2., 2.).step(0.001);
@@ -35,7 +52,15 @@ var colorA = new THREE.Vector3( guiData.colorA[ 0 ] / 255, guiData.colorA[ 1 ] /
 var colorB = new THREE.Vector3( guiData.colorB[ 0 ] / 255, guiData.colorB[ 1 ] / 255, guiData.colorB[ 2 ] / 255 );
 
 
+var camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
+camera.position.y = 0;
+camera.position.z = 5;
+
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableRotate = false;
+var domEvents	= new THREEx.DomEvents(camera, renderer.domElement);
+
+
 
 
 material = new THREE.ShaderMaterial( {
@@ -51,6 +76,10 @@ material = new THREE.ShaderMaterial( {
     "t": { type: "f", value: guiData.t },
     "power": { type: "f", value: guiData.power },
     "rotate": { type: "f", value: guiData.rotate },
+    "cR": { type: "f", value: guiData.cR },
+    "cI": { type: "f", value: guiData.cI },
+    "cJ": { type: "f", value: guiData.cJ },
+    "cK": { type: "f", value: guiData.cK },
   },
   fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
   vertexShader: document.getElementById( 'vertexShader' ).textContent,
@@ -69,22 +98,53 @@ function onWindowResize() {
     renderer.setSize(width, height);
   }
 
+//Double clicking for starting rotation
+domEvents.addEventListener(plane, 'dblclick', function(event){
+  console.log('you double clicked on mesh', plane)
+  rotationFlag = !rotationFlag;
+
+  if (!rotationFlag){
+    dragControls.activate();
+
+  }
+}, false)
+
+var dragControls = new THREE.DragControls( [plane], camera, renderer.domElement );
+dragControls.addEventListener( 'dragstart', function ( event ) {
+  if(rotationFlag){
+    dragControls.deactivate();
+  }
+} );
+
+
 function animate() {
+  
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 
     var time = performance.now() * 0.0005;
     material.uniforms[ "time" ].value = time;
-    material.uniforms[ "camX" ].value = guiData.camX;
-    material.uniforms[ "camY" ].value = guiData.camY;
+    material.uniforms[ "cR" ].value = guiData.cR;
+    material.uniforms[ "cI" ].value = guiData.cI;
+    material.uniforms[ "cJ" ].value = guiData.cJ;
+    material.uniforms[ "cK" ].value = guiData.cK;
+
     material.uniforms[ "camZ" ].value = guiData.camZ;
+    material.uniforms[ "camY" ].value = guiData.camY;
+    material.uniforms[ "camX" ].value = guiData.camX;
+    
     material.uniforms[ "zoom" ].value = guiData.zoom;
     material.uniforms[ "colorA" ].value = guiData.colorA;
     material.uniforms[ "colorB" ].value = guiData.colorB;
     material.uniforms[ "t" ].value = guiData.t;
     material.uniforms[ "power" ].value = guiData.power;
-    material.uniforms[ "rotate" ].value = guiData.rotate;
+  
+    
+    if (rotationFlag){
+     material.uniforms[ "rotate" ].value += 0.001 ;
+    }else{
+      material.uniforms[ "rotate" ].value = guiData.rotate;
+    }
   } 
 
-window.addEventListener('resize', onWindowResize, false);
 animate();
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('views'));
+app.use(express.static('public/views'));
+app.use(express.static('public'));
 app.use(express.static('node_modules/three/build'));
 app.use(express.static(__dirname +'/node_modules/dat.gui/build'))
 
@@ -12,5 +13,5 @@ var listener = app.listen(8080, function () {
 });
 
 app.get("/3d", (request, response) => {
-  response.sendFile(__dirname + "/views/julia-3d.html");
+  response.sendFile(__dirname + "/public/views/julia-3d.html");
 });
